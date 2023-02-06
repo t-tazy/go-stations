47f6f9cae1d0ec789520621ff0db592fbfca5d74
t-tazy <takumin390511@gmail.com>
2023-02-06 18:22:32 +0900

    refactor(recovery middleware): HTTPレスポンスの内容を変更

--- handler/middleware/recovery.go
+++ handler/middleware/recovery.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -9,7 +11,14 @@ func RecoveryMiddleware(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if r := recover(); r != nil {
-				fmt.Println("recovery")
+				jsonBody, _ := json.Marshal(map[string]string{
+					"error": fmt.Sprintf("%v", r),
+				})
+				w.Header().Set("Content-Type", "application/json; charset=utf-8")
+				w.WriteHeader(http.StatusInternalServerError) // 500
+				w.Write(jsonBody)
+
+				log.Printf("%v", r)
 			}
 		}()
 		h.ServeHTTP(w, r)
